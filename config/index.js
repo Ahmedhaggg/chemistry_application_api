cb0b304ae211ef661196276a0167f36f2f7da64f
ahmed haggag <ahmedhaggagrady@gmail.com>
2022-10-17 16:48:55 +0200

    add port env variable

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 let JWT_SECRET = process.env.JWT_SECRET;
 let BCRYPT_SALT = process.env.BCRYPT_SALT;
 let DB_URL_CON = process.env.DB_URL_CON
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 5000;
+
 module.exports = { JWT_SECRET, BCRYPT_SALT, DB_URL_CON, PORT };
